9c14390010869308718c54417523051035001916
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-04-16 21:13:01 +0500

    Add Some More Functions

--- index.js
+++ index.js
@@ -1,50 +1,75 @@
+#! /usr/bin/env node
 import inquirer from 'inquirer';
 import chalk from 'chalk';
-console.log(chalk.magentaBright.bold('\n \t Welcome To codeWithSahil - Result Check Application \n'));
+console.log(chalk.magentaBright.bold.underline('\t "Welcome To codeWithSahil - Percentage Or Grade Check Application" \n'));
 let condition = true;
 while (condition) {
-    const marksAns = await inquirer.prompt([
+    const answer = await inquirer.prompt([
         {
-            name: 'marks',
-            type: 'number',
-            message: chalk.greenBright.bold('Enter Your Percentage : (Without Sign %)'),
-        },
-    ]);
-    if (marksAns.marks >= 90 && marksAns.marks <= 100) {
-        console.log(chalk.yellowBright.bold('\n \t Your Result Is A+ Grade \n'));
-    }
-    else if (marksAns.marks >= 80 && marksAns.marks <= 89.9) {
-        console.log(chalk.yellowBright.bold('\n \t Your Result Is A Grade \n'));
-        ;
-    }
-    else if (marksAns.marks >= 70 && marksAns.marks <= 79.9) {
-        console.log(chalk.yellowBright.bold('\n \t Your Result Is B Grade \n'));
-        ;
-    }
-    else if (marksAns.marks >= 60 && marksAns.marks <= 69.9) {
-        console.log(chalk.yellowBright.bold('\n \t Your Result Is C Grade \n'));
-        ;
-    }
-    else if (marksAns.marks >= 50 && marksAns.marks <= 59.9) {
-        console.log(chalk.yellowBright.bold('\n \t Your Result Is D Grade \n'));
-        ;
-    }
-    else if (marksAns.marks >= 40 && marksAns.marks <= 49.9) {
-        console.log(chalk.yellowBright.bold('\n \t Your Result Is F Grade \n'));
-        ;
-    }
-    else {
-        console.log(chalk.redBright.bold('\n \t You Are Faild \n'));
-    }
-    let againAns = await inquirer.prompt([
-        {
-            name: 'again',
-            type: 'confirm',
-            message: chalk.blueBright.bold('Do You Want Again Check Your Result'),
-            default: false
+            name: 'userAns',
+            type: 'list',
+            message: chalk.cyanBright.bold('Select Option'),
+            choices: ['Check Percentage', 'Check Result', 'Exit']
         }
     ]);
-    condition = againAns.again;
+    if (answer.userAns === 'Check Percentage') {
+        const percentageAns = await inquirer.prompt([
+            {
+                name: 'obtainMarks',
+                type: 'number',
+                message: chalk.greenBright.bold('Enter Your Obtain Marks'),
+            },
+            {
+                name: 'totalMarks',
+                type: 'number',
+                message: chalk.greenBright.bold('Enter Your Total Marks')
+            }
+        ]);
+        let mark1 = (percentageAns.obtainMarks);
+        let mark2 = (percentageAns.totalMarks);
+        let percentage = (mark1 / mark2) * 100;
+        console.log(chalk.yellowBright.bold.underline(' Your Percentage Is :' + Math.round(percentage) + '%'));
+    }
+    else if (answer.userAns === 'Check Result') {
+        const marksAns = await inquirer.prompt([
+            {
+                name: 'marks',
+                type: 'number',
+                message: chalk.greenBright.bold('Enter Your Percentage Number:'),
+            },
+        ]);
+        if (marksAns.marks >= 90 && marksAns.marks <= 100) {
+            console.log(chalk.yellowBright.bold.underline('\t Your Result Is A+ Grade \n'));
+        }
+        else if (marksAns.marks >= 80 && marksAns.marks <= 89.9) {
+            console.log(chalk.yellowBright.bold.underline('\t Your Result Is A Grade \n'));
+            ;
+        }
+        else if (marksAns.marks >= 70 && marksAns.marks <= 79.9) {
+            console.log(chalk.yellowBright.bold.underline('\t Your Result Is B Grade \n'));
+            ;
+        }
+        else if (marksAns.marks >= 60 && marksAns.marks <= 69.9) {
+            console.log(chalk.yellowBright.bold.underline('\t Your Result Is C Grade \n'));
+            ;
+        }
+        else if (marksAns.marks >= 50 && marksAns.marks <= 59.9) {
+            console.log(chalk.yellowBright.bold.underline('\t Your Result Is D Grade \n'));
+            ;
+        }
+        else if (marksAns.marks >= 40 && marksAns.marks <= 49.9) {
+            console.log(chalk.yellowBright.bold.underline('\t Your Result Is F Grade \n'));
+            ;
+        }
+        else {
+            console.log(chalk.redBright.bold.underline('\t You Are Faild \n'));
+        }
+    }
+    else if (answer.userAns === 'Exit') {
+        condition = false;
+    }
+    else {
+        console.log(chalk.redBright.bold.underline('\t Please Enter Valid Input \n'));
+    }
 }
-;
-console.log(chalk.grey.bold('\n \t Thanks You \n'));
+console.log(chalk.grey.bold.underline('\t Thanks You \n'));
